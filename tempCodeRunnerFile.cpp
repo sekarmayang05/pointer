e93f57df0ff95cea40836f0276e7686eba18d6bf
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-05-06 18:34:11 +0700

    membuat class lingkaran

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,15 @@
+class Lingkaran
+{
+public:
+    int jarijari;
+
+    void input()
+    {
+        cout << "Masukkan jari-jari : ";
+        cin >> jarijari;
+    }
+    int luasLingkaran(int r)
+    {
+        return 3.24 * r * r;
+    }
+};
