90c871464f464cee61e258295973f41dcfc1911e
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-05-06 18:28:42 +0700

    membuat akses modifair

---
+++ file1.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+using namespace std;
+
+class PersegiPanjang
+{
+public: // akses modifier
+    int panjang, lebar;
+
+public:
+    void input()
+    {
+        cout << "Masukkan panjang : ";
+        cin >> panjang;
+        cout << "Masukan lebar : ";
+        cin >> lebar;
+    }
+
+    int luasPp(int a,int b)
+    {
+        return a * b;
+    }
+    // Lingkaran ol;
+    
+};
